5a4eb328c8254dd024857bb0310e624718a83a6e
Allossy01 <ademecom24@gmail.com>
2026-09-12 01:56:40 +0300

    auto-update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://psqyilfdqlbjakwornnv.supabase.co';
-const supabaseKey = 'sb_publishable__Rc8PiMjS2qwylZzbsdsVw_Od1X3G_A';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzcXlpbGZkcWxiamFrd29ybm52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNTMxNjksImV4cCI6MjEwNDcyOTE2OX0.z7uAt1klFjtEueous02qPkzaKMwYm_IL-Ehz47GKKWg';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
