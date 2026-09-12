3afd6a949b690ebf285e0b963e5a30b56bf682dd
Allossy01 <ademecom24@gmail.com>
2026-09-12 20:33:39 +0300

    auto-update

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -166,7 +166,7 @@ export default function Contact() {
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3324.0!2d-7.5559381!3d33.5875157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda7cddc57ab987d%3A0x6224df2107f716dd!2sNew%20Star%20Travel%20Agency!5e0!3m2!1sfr!2sma!4v1"
                 width="100%"
                 height="200"
-                style={{ border: 0, display: 'block', filter: 'invert(90%) hue-rotate(180deg)' }}
+                style={{ border: 0, display: 'block' }}
                 allowFullScreen
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
